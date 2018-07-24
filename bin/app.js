78ab6edadf897a09af9593841f11e1263a77ae3c
mjkim <mjkim01@wemakeprice.com>
2018-07-24 17:06:22 +0900

    project start

---
+++ bin/app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', async (req, res, next) => {
+  res.status(200).send({msg: 'Hello Node Server.'});
+});
+
+const server = http.createServer(app);
+
+server.listen(8000, () => {console.log('Server liston start')});
