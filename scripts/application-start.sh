738816bfaf5b2e3ac03975127887cffd3b3aafee
mjkim <mjkim01@wemakeprice.com>
2018-07-24 18:56:07 +0900

    deploy 수정 3

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-export NVM_DIR="$HOME/.nvm"
-
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
 cd /home/ec2-user/node-server
 
 pm2 stop bin/app.js
