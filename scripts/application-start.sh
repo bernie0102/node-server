ffd3977ed93dc1fbb1ea0b212b6b566433488aa1
mjkim <mjkim01@wemakeprice.com>
2018-07-26 16:39:23 +0900

    deploy 27

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -4,9 +4,9 @@ export PATH=$PATH:/usr/local/bin/
 
 cd /home/ec2-user/node-server
 
-if [[ $DEPLOYMENT_GROUP_NAME = *"production"* ]]; then
+if [ $DEPLOYMENT_GROUP_NAME == "production" ]; then
     export NODE_ENV="production"
-elif [[ $DEPLOYMENT_GROUP_NAME = *"staging"* ]]; then
+elif [ $DEPLOYMENT_GROUP_NAME == "staging" ]; then
     export NODE_ENV="staging"
 else
     export NODE_ENV="development"
