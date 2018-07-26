4051d54929b2f14b8bda153044d1197ece30f042
mjkim <mjkim01@wemakeprice.com>
2018-07-26 16:55:19 +0900

    deploy 29

--- bin/app.js
+++ bin/app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/', async (req, res, next) => {
-  res.status(200).send({msg: `Hello Node Server env : ${process.env.NODE_ENV} 28`});
+  res.status(200).send({msg: `Hello Node Server env : ${process.env.NODE_ENV} 29`});
 });
 
 const server = http.createServer(app);
