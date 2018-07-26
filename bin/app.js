ee3822e7ad46abc8efcd13ed822bcfade99f3556
mjkim <mjkim01@wemakeprice.com>
2018-07-26 15:24:09 +0900

    deploy 23

--- bin/app.js
+++ bin/app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/', async (req, res, next) => {
-  res.status(200).send({msg: 'Hello Node Server 22'});
+  res.status(200).send({msg: 'Hello Node Server 23'});
 });
 
 const server = http.createServer(app);
